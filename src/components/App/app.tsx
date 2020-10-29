6ca46f5d94e8c80192bf6651158eb60c9c96fb61
Marco Kruse <marco.kruse@devahead.de>
2020-10-29 16:15:53 +0100

    Add example

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,29 +1,61 @@
-import React from 'react'
-import './app.scss'
+import React from 'react';
+import './app.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <div id="sidebar">
-        <ul id="mainMenu">
-          <li>
-            <a href="placeholder">Link 1</a>
+    <div className="layout">
+      <div className="layout__sidebar">
+        <ul className="sidemenu">
+          <li className="sidemenu__item">
+            <a className="sidemenu__link" href="placeholder">
+              Link 1
+            </a>
           </li>
-          <li className="active">
-            <a href="placeholder">Link 2</a>
+          <li className="sidemenu__item sidemenu__item--active">
+            <a className="sidemenu__link" href="placeholder">
+              Link 2
+            </a>
+            <a className="button button--small" href="placeholder">
+              Click me
+            </a>
           </li>
-          <li>
-            <a href="placeholder">Link 3</a>
+          <li className="sidemenu__item">
+            <a className="sidemenu__link" href="placeholder">
+              Link 3
+            </a>
+          </li>
+        </ul>
+        <ul className="sidemenu sidemenu--secondary">
+          <li className="sidemenu__item">
+            <a className="sidemenu__link" href="placeholder">
+              Another link 1
+            </a>
+          </li>
+          <li className="sidemenu__item sidemenu__item--active">
+            <a className="sidemenu__link" href="placeholder">
+              Another link 2
+            </a>
+            <a className="button" href="placeholder">
+              Click me
+            </a>
+          </li>
+          <li className="sidemenu__item">
+            <a className="sidemenu__link" href="placeholder">
+              Another link 3
+            </a>
           </li>
         </ul>
       </div>
-      <div className="main">
+      <div className="layout__main">
         <a className="button" href="placeholder">
+          Click me
+        </a>
+        <a className="button button--small" href="placeholder">
           Click me
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
